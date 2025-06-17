d0cf03732e102884eeb2b235473c2c7b98f5610e
Daniil Sivovolenko <Sivovolenkodaniil@gmail.com>
2025-06-17 12:51:30 +0200

    hopefully works

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // Ensure you import HashRouter!
-import App from "./App";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App'  // or route.tsx if that file is your entry point
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-    <HashRouter>
-        <App />
-    </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
